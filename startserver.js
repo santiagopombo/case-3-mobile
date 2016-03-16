76391742cdce72ddbd055654d9bdd99b73ff6f5c
Garkusha <nik.garkusha@hpe.com>
2016-03-15 18:53:09 -0700

    updated port output

--- startserver.js
+++ startserver.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
-console.log ("PORT: " + process.env.PORT || 3210);
+var aport = process.env.PORT || 8080;
+console.log ("PORT: " + aport);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -9,4 +10,4 @@ app.get('/provider', function (req, res) {
     res.send(process.env.PROVIDER);
 });
 
-app.listen(process.env.PORT || 3210);
+app.listen(process.env.PORT || 8080);
